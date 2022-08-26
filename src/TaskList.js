8644ab20838b4ac871823e39fbeb18ed55ca2b05
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2022-08-27 00:17:57 +0400

    Update TaskList.js

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,16 +1,16 @@
-// 1
+
 import React from 'react'
 import Task from './Task'
 import { useTasks } from './TaskProvider'
 
 export default function TaskList() {
-  // 2
+
   const { tasks } = useTasks()
   return (
     <table>
       <tbody>
         {
-          // 3
+         
           tasks.map((task, i) => 
             <Task key={i} {...task} />
           )
