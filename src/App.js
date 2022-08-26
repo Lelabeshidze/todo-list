03bf667cf775e12eacf0c0e8ef137e740a0fea45
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2022-08-27 00:16:15 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import '@fontsource/roboto/300.css';
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-// import '@fontsource/roboto/700.css';
-
-// // 1
-// import React from 'react'
-
-// import NewTaskForm from './NewTaskForm'
-// import TaskList from './TaskList'
-
-// function App() {
-//   // 2
-//   return (
-//     <div className="container">
-//       <h1 className="title">Todo List</h1>
-//       <NewTaskForm />
-//       <TaskList />
-//     </div>
-//   )
-// }
-
-// export default App;
+import logo from './logo.svg';
 import './App.css';
-import Form from "./Form";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+import React from 'react'
+
+import NewTaskForm from './NewTaskForm'
+import TaskList from './TaskList'
 
 function App() {
+  
   return (
-    <div className="wrapper">
-      <h1>Todo List</h1>
-      <Form/>
+    <div className="container">
+      <h1 className="title">Todo List</h1>
+      <NewTaskForm />
+      <TaskList />
     </div>
-  );
+  )
 }
 
 export default App;
