9e3557499cec4c74933b06bc2fac32a19d4b50a5
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2022-08-27 00:16:56 +0400

    Update NewTaskForm.js

--- src/NewTaskForm.js
+++ src/NewTaskForm.js
@@ -3,11 +3,11 @@ import React, { useState } from 'react'
 import { useTasks } from './TaskProvider'
 
 export default function NewTaskForm() {
-  // 2
+  
   const [task, setTask] = useState('')
   const { addTask } = useTasks()
 
-  // 3
+  
   const submit = e => {
     e.preventDefault()
     addTask(task)
@@ -15,7 +15,7 @@ export default function NewTaskForm() {
   }
 
   return (
-    // 4
+    
     <form onSubmit={submit}>
     
       <input type="text" value={task} placeholder="Add task" onChange={e => setTask(e.target.value)} required />
