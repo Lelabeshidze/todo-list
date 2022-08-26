7b37ceed0abcc51c923cd026f6c431920bb0d837
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2022-08-27 00:17:20 +0400

    Update Task.js

--- src/Task.js
+++ src/Task.js
@@ -1,12 +1,12 @@
-// 1
+
 import React from "react";
 import { useTasks } from "./TaskProvider";
 
 export default function Task({ id, task, complete }) {
-	// 2
+	
 	const { setStatusTask } = useTasks();
 
-	// 3
+	
 	const checkTask = (e) => setStatusTask(id, e.target.checked);
 
 	return (
